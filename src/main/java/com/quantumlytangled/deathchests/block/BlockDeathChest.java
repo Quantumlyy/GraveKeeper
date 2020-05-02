e765d366766bd75fedbb59ca8976db720301bc75
Nejc <ethoarx@gmail.com>
2020-05-02 23:38:35 +0200

    Heh

--- src/main/java/com/quantumlytangled/deathchests/block/BlockDeathChest.java
+++ src/main/java/com/quantumlytangled/deathchests/block/BlockDeathChest.java
@@ -1,7 +1,13 @@
 package com.quantumlytangled.deathchests.block;
 
+import com.quantumlytangled.deathchests.DeathChests;
+import com.quantumlytangled.deathchests.tile.TileDeathChest;
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class BlockDeathChest extends Block {
     public BlockDeathChest () {
@@ -9,5 +15,11 @@ public class BlockDeathChest extends Block {
 
         setBlockUnbreakable();
         setRegistryName("DeathChest");
+        setTranslationKey(DeathChests.MODID + ".death_chest");
+    }
+
+    public TileEntity createTileEntity(World world, IBlockState state)
+    {
+        return new TileDeathChest();
     }
 }
--- src/main/java/com/quantumlytangled/deathchests/tile/TileDeathChest.java
+++ src/main/java/com/quantumlytangled/deathchests/tile/TileDeathChest.java
@@ -3,5 +3,7 @@ package com.quantumlytangled.deathchests.tile;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileDeathChest extends TileEntity {
-
+    private String owner = "none";
+    private long creationDate = 0;
+    public boolean isSecure = true;
 }
