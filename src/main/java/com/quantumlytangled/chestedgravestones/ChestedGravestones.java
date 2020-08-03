a4977c35c9188609f36388c8dc94315accb05efd
LemADEC <lemadec.fr@gmail.com>
2020-08-03 20:01:23 +0200

    Fixed mod instanciation

--- src/main/java/com/quantumlytangled/chestedgravestones/ChestedGravestones.java
+++ src/main/java/com/quantumlytangled/chestedgravestones/ChestedGravestones.java
@@ -1,33 +1,30 @@
 package com.quantumlytangled.chestedgravestones;
 
 import com.quantumlytangled.chestedgravestones.core.Registration;
-import javax.annotation.Nonnull;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ChestedGravestones.MODID, name = ChestedGravestones.NAME, version = ChestedGravestones.VERSION)
+@Mod(modid = ChestedGravestones.MODID,
+    name = ChestedGravestones.NAME,
+    version = ChestedGravestones.VERSION)
 public class ChestedGravestones {
 
   public static final String MODID = "chestedgravestones";
   public static final String NAME = "ChestedGravestones";
   public static final String VERSION = "@VERSION@";
 
-  private static final ChestedGravestones INSTANCE = new ChestedGravestones();
+  @Instance(ChestedGravestones.MODID)
+  public static ChestedGravestones INSTANCE;
 
   private final Registration registration;
 
-  private ChestedGravestones() {
+  public ChestedGravestones() {
     registration = new Registration();
     MinecraftForge.EVENT_BUS.register(registration);
-  }
-
-  @Nonnull
-  @Mod.InstanceFactory
-  public static ChestedGravestones instance() {
-    return INSTANCE;
   }
 
   @EventHandler
