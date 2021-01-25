04fee927dbd89911c37314859c8db4418a6e33e6
QuantumlyTangled <ethoarx@gmail.com>
2021-01-25 13:19:45 +0100

    refactor: armor -> armour, to match enum entry

--- src/main/java/com/quantumlytangled/gravekeeper/compatibility/CompatArmour.java
+++ src/main/java/com/quantumlytangled/gravekeeper/compatibility/CompatArmour.java
@@ -6,11 +6,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import com.quantumlytangled.gravekeeper.util.InventoryType;
 
-public class CompatArmor implements ICompatInventory {
+public class CompatArmour implements ICompatInventory {
 
-  private static final CompatArmor INSTANCE = new CompatArmor();
+  private static final CompatArmour INSTANCE = new CompatArmour();
 
-  public static CompatArmor getInstance() {
+  public static CompatArmour getInstance() {
     return INSTANCE;
   }
 
