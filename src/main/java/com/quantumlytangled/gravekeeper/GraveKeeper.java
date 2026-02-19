b24eab9429fbb269fdef342461dd2c7b3fc66861
Nejc Drobnic <nejc@flashnet.xyz>
2026-02-19 10:44:02 +0100

    init

--- src/main/java/com/quantumlytangled/gravekeeper/GraveKeeper.java
+++ src/main/java/com/quantumlytangled/gravekeeper/GraveKeeper.java
@@ -1,111 +1,131 @@
 package com.quantumlytangled.gravekeeper;
 
-import com.quantumlytangled.gravekeeper.block.BlockDeathChest;
-import com.quantumlytangled.gravekeeper.block.TileDeathChest;
-import com.quantumlytangled.gravekeeper.command.CommandList;
-import com.quantumlytangled.gravekeeper.command.CommandRestore;
-import com.quantumlytangled.gravekeeper.core.DeathHandler;
-import com.quantumlytangled.gravekeeper.util.LoggerPrintStream;
+import org.slf4j.Logger;
 
-import javax.annotation.Nonnull;
-import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.util.ResourceLocation;
+import com.mojang.logging.LogUtils;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.Logger;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.Minecraft;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
 
-@Mod(modid = GraveKeeper.MODID,
-    name = GraveKeeper.NAME,
-    version = GraveKeeper.VERSION)
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
+@Mod(GraveKeeper.MODID)
 public class GraveKeeper {
-  
-  public static final String MODID = "gravekeeper";
-  public static final String NAME = "GraveKeeper";
-  public static final String VERSION = "@VERSION@";
-  
-  @Instance(GraveKeeper.MODID)
-  public static GraveKeeper INSTANCE;
-  
-  public static Logger logger;
-  public static LoggerPrintStream printStreamError;
-  public static LoggerPrintStream printStreamWarn;
-  public static LoggerPrintStream printStreamInfo;
-  public static Block blockDeathChest;
-  public static Item itemDeathCertificate;
-  
-  public GraveKeeper() {
-    MinecraftForge.EVENT_BUS.register(this);
-    MinecraftForge.EVENT_BUS.register(new DeathHandler());
-  }
-  
-  @EventHandler
-  private void preInit(@Nonnull final FMLPreInitializationEvent event) {
-    logger = event.getModLog();
-    printStreamError = new LoggerPrintStream(Level.ERROR);
-    printStreamWarn = new LoggerPrintStream(Level.WARN);
-    printStreamInfo = new LoggerPrintStream(Level.INFO);
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "gravekeeper";
+    // Directly reference a slf4j logger
+    public static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "gravekeeper" namespace
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "gravekeeper" namespace
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "gravekeeper" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    GraveKeeperConfig.onFMLpreInitialization(event.getModConfigurationDirectory());
+    // Creates a new Block with the id "gravekeeper:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    // Creates a new BlockItem with the id "gravekeeper:example_block", combining the namespace and path
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
 
-    blockDeathChest = new BlockDeathChest();
+    // Creates a new food item with the id "gravekeeper:example_id", nutrition 1 and saturation 2
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    itemDeathCertificate = new Item()
-        .setRegistryName(GraveKeeper.MODID + ":death_certificate")
-        .setTranslationKey("gravekeeper.death_certificate")
-        .setCreativeTab(CreativeTabs.TOOLS);
-  }
-  
-  @EventHandler
-  private void preInit(@Nonnull final FMLPostInitializationEvent event) {
-    GraveKeeperConfig.onFMLpostInitialization();
-  }
-  
-  @SubscribeEvent
-  public void registerBlocks(@Nonnull final RegistryEvent.Register<Block> event) {
-    event.getRegistry().register(blockDeathChest);
-    GameRegistry.registerTileEntity(TileDeathChest.class,
-                                    new ResourceLocation(GraveKeeper.MODID, "death_chest"));
-  }
-  
-  @SubscribeEvent
-  public void onRegisterItems(@Nonnull final RegistryEvent.Register<Item> event) {
-    final ItemBlock itemBlock = new ItemBlock(blockDeathChest);
-    ResourceLocation resourceLocation = blockDeathChest.getRegistryName();
-    assert resourceLocation != null;
-    itemBlock.setRegistryName(blockDeathChest.getRegistryName()).setCreativeTab(CreativeTabs.TOOLS);
-    event.getRegistry().register(itemBlock);
-    if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-      ModelLoader.setCustomModelResourceLocation(itemBlock, 0, new ModelResourceLocation(resourceLocation, "inventory"));
+    // Creates a creative tab with the id "gravekeeper:example_tab" for the example item, that is placed after the combat tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.gravekeeper")) //The language key for the title of your CreativeModeTab
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+            }).build());
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public GraveKeeper(IEventBus modEventBus, ModContainer modContainer) {
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        BLOCKS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ITEMS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so tabs get registered
+        CREATIVE_MODE_TABS.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (GraveKeeper) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
+
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
+
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
-    
-    resourceLocation = itemDeathCertificate.getRegistryName();
-    assert resourceLocation != null;
-    event.getRegistry().register(itemDeathCertificate);
-    if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-      ModelLoader.setCustomModelResourceLocation(itemDeathCertificate, 0, new ModelResourceLocation(resourceLocation, "inventory"));
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+        }
+
+        LOGGER.info("{}{}", Config.MAGIC_NUMBER_INTRODUCTION.get(), Config.MAGIC_NUMBER.getAsInt());
+
+        Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
-  }
-  
-  @EventHandler
-  public void onFMLServerStarting(@Nonnull final FMLServerStartingEvent event) {
-    event.registerServerCommand(new CommandList());
-    event.registerServerCommand(new CommandRestore());
-  }
+
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(EXAMPLE_BLOCK_ITEM);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = GraveKeeper.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    static class ClientModEvents {
+        @SubscribeEvent
+        static void onClientSetup(FMLClientSetupEvent event) {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
 }
